24323a28cade22652cb5a6d5b591530ca5b0eb69
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-05-14 22:05:19 +0500

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -50,7 +50,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `http://34.59.36.165:3000/activation/${activationToken}`;
 
     // send email to user
     try {
