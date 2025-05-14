d98768c1f0d16e305c534a5f2c45a6e607e5bb59
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-05-14 18:33:27 +0500

    Update Database.js

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
+    .connect("mongodb+srv://user:password@cluster0.mongodb.net/qazprovide", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then((data) => {
-      console.log(`mongod connected with server: ${data.connection.host}`);
+      console.log(`MongoDB connected with server: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
     });
 };
 
