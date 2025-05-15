caed45a7b1781f52c849b4e99c780702672c88f2
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-05-15 14:57:41 +0500

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,8 +7,8 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none",
-    secure: true,
+    sameSite: "lax",
+    secure: false,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
