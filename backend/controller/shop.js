c7fd873155e85e56236d16355dcda1b94b281e2a
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-05-15 15:33:26 +0500

    Update shop.js

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -45,7 +45,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    const activationUrl = `http://34.59.36.165:3000/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
