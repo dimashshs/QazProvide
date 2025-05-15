ef97f0bbdeb140bb91edb07cae83888f14717a62
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-05-15 11:37:00 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import axios from 'axios';
-
-axios.defaults.withCredentials = true; 
 
 import { Provider } from "react-redux";
 import Store from "./redux/store";
