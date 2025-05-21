8a82f1c92233ecb4787d1dd2440a132f25e3bc07
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-05-21 12:38:49 +0500

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,11 +1,2 @@
-// VM-1 (backend на 34.59.36.165)
 export const server = "http://34.59.36.165:5000/api/v2";
 export const backend_url = "http://34.59.36.165:5000/";
-
-// VM-2 (backend на 34.59.111.109)
-export const server = "http://34.59.111.109:5000/api/v2";
-export const backend_url = "http://34.59.111.109:5000/";
-
-// VM-3 (backend на 34.46.168.22)
-export const server = "http://34.46.168.22:5000/api/v2";
-export const backend_url = "http://34.46.168.22:5000/";
